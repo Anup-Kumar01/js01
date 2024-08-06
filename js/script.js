b177a78630c41626672f6b21a6dfc61d8923024b
Anup kumar <133348659+Anup-Kumar01@users.noreply.github.com>
2024-08-06 23:03:56 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,49 @@
+// example 01
+function showText01(){
+	document.getElementById('s_text01').innerHTML="Hello SSB-619";	
+}
+
+// example 02
+function showText02(){
+	document.getElementById('s_text02').innerHTML="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";	
+}
+
+// example 03
+function showText03(){
+	document.getElementById('s_text03').innerHTML="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.";	
+}
+
+// example 04
+function showText04(){
+	document.getElementById('s_text04').innerHTML="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";	
+}
+
+// example 05
+function showText05(){
+	document.getElementById('s_text05').innerHTML="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";	
+}
+
+// example 06
+function showText06(){
+	document.getElementById('s_text06').innerHTML="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";	
+}
+
+// example 07
+function showText07(){
+	document.getElementById('s_text07').innerHTML="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";	
+}
+
+// example 08
+function showText08(){
+	document.getElementById('s_text08').innerHTML="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";	
+}
+
+// example 09
+function showText09(){
+	document.getElementById('s_text09').innerHTML="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";	
+}
+
+// example 10
+function showText10(){
+	document.getElementById('s_text10').innerHTML="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";	
+}
